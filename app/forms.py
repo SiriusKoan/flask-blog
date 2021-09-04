dd8fcacdcce8ca078175256c29a1252f560e11cf
SiriusKoan <k.peihsun@gmail.com>
2021-09-04 22:55:04 +0800

    Add some forms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Length, EqualTo, Regexp
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp, ValidationError
+from flask_pagedown.fields import PageDownField
 
 
 class LoginForm(FlaskForm):
@@ -12,6 +13,7 @@ class LoginForm(FlaskForm):
         "Password", validators=[DataRequired()], render_kw={"placeholder": "Password"}
     )
     submit = SubmitField("Login")
+
 
 class RegisterForm(FlaskForm):
     username = StringField(
@@ -46,3 +48,61 @@ class RegisterForm(FlaskForm):
         "Email", validators=[DataRequired()], render_kw={"placeholder": "Email"}
     )
     submit = SubmitField("Register")
+
+
+class UserSettingForm(FlaskForm):
+    password = PasswordField(
+        "Password",
+        render_kw={"placeholder": "Password"},
+    )
+    email = EmailField(
+        "Email", validators=[DataRequired()], render_kw={"placeholder": "Email"}
+    )
+    submit = SubmitField("Update")
+
+    def validate_password(self, field):
+        if type(field.data) is str:
+            if field.data != "" and len(field.data) < 6:
+                raise ValidationError(
+                    "The password must contain at least 6 characters."
+                )
+        else:
+            raise ValidationError("Invalid type.")
+
+
+class AddPostForm(FlaskForm):
+    content = PageDownField("Enter your markdown")
+    submit = SubmitField("Submit")
+
+
+class AddCommentFrom(FlaskForm):
+    content = StringField("Comment", render_kw={"placeholder": "Comment"})
+    submit = SubmitField("Comment")
+
+
+class AddUserForm(FlaskForm):
+    username = StringField(
+        "Username",
+        validators=[
+            DataRequired(),
+            Length(min=4, max=30, message="The name should be 4 to 30 letters long."),
+            Regexp(
+                "[a-zA-Z0-9_]+",
+                message="Only letters, numbers and underscore are allowed in username.",
+            ),
+        ],
+        render_kw={"placeholder": "Username"},
+    )
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=6, message="The password must contain at least 6 characters."),
+        ],
+        render_kw={"placeholder": "Password"},
+    )
+    email = EmailField(
+        "Email", validators=[DataRequired()], render_kw={"placeholder": "Email"}
+    )
+    is_admin = BooleanField("The user is an admin")
+    submit = SubmitField("Add")
