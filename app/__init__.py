12e7c9d6fc0050b1efcfa0b5d43eff8d54ec01c1
SiriusKoan <k.peihsun@gmail.com>
2021-09-05 15:37:49 +0800

    Add pagedown and markdown

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_pagedown import PageDown
+from flaskext.markdown import Markdown
 from .database import db
 from .config import configs
 
 login_manager = LoginManager()
 mail = Mail()
+pagedown = PageDown()
 
 
 def create_app(env):
@@ -15,6 +18,8 @@ def create_app(env):
     login_manager.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    pagedown.init_app(app)
+    Markdown(app)
 
     # Blueprint
     # from .main import main_bp
