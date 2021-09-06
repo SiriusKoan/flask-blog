99452c4384d0a1921b88db7de490cf5216b8748e
SiriusKoan <k.peihsun@gmail.com>
2021-09-06 20:37:08 +0800

    Finish main_bp

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,18 @@
+from flask import render_template, redirect, url_for
 from werkzeug.exceptions import HTTPException
 from . import main_bp
 
 
 @main_bp.route("/", methods=["GET"])
 def index_page():
-    pass
+    return render_template("index.html")
 
 
 @main_bp.app_errorhandler(401)
 def handler_401(e):
-    pass
+    return redirect(url_for("user.login_page"))
 
 
 @main_bp.app_errorhandler(HTTPException)
 def handler(e):
-    pass
+    return render_template("error.html", code=e.code, name=e.name), e.code
